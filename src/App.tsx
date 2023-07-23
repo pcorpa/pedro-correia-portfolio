d186232f009c0263db4afc7b0e14d86a9ea9092a
“pcorpa” <p.correia.pastorini@gmail.com>
2023-07-23 19:22:20 -0300

    feat: react ts materialui

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+
+function App() {
+  const [value, setValue] = React.useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
+  console.log(value);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+        <Box sx={{ width: 200 }}>
+          <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            <VolumeDown />
+            <Slider aria-label="Volume" value={value} onChange={handleChange} />
+            <VolumeUp />
+          </Stack>
+          <Slider disabled defaultValue={30} aria-label="Disabled slider" />
+        </Box>
+      </header>
+    </div>
+  );
+}
+
+export default App;
